fba61b0716cb6775fbba90a4aa70e5121936c242
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-30 00:21:09 -0300

    Implementacao de service para pagamento.

---
+++ backend/insurance-payment-api/src/main/java/com/lambdasys/insurance/payment/service/PaymentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.lambdasys.insurance.payment.service;
+
+import com.lambdasys.insurance.checkout.event.CheckoutCreatedEvent;
+import com.lambdasys.insurance.payment.entity.PaymentEntity;
+import com.lambdasys.insurance.payment.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    @Override
+    public Optional<PaymentEntity> create(CheckoutCreatedEvent checkoutCreatedEvent) {
+        final PaymentEntity paymentEntity = PaymentEntity.builder()
+                .checkoutCode(checkoutCreatedEvent.getCheckoutCode())
+                .code(UUID.randomUUID().toString())
+                .build();
+        paymentRepository.save(paymentEntity);
+        return Optional.of(paymentEntity);
+    }
+}
