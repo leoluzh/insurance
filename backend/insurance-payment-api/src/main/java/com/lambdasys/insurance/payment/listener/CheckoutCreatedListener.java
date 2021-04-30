03db9397da0185626d001775ac238cac6b2e6ebc
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-04-30 00:28:29 -0300

    Listesner para envento de criacao de checkout e envio de notificacao de pagamento.

---
+++ backend/insurance-payment-api/src/main/java/com/lambdasys/insurance/payment/listener/CheckoutCreatedListener.java
@@ -0,0 +1,33 @@
+package com.lambdasys.insurance.payment.listener;
+
+import com.lambdasys.insurance.checkout.event.CheckoutCreatedEvent;
+import com.lambdasys.insurance.payment.entity.PaymentEntity;
+import com.lambdasys.insurance.payment.event.PaymentCreatedEvent;
+import com.lambdasys.insurance.payment.service.PaymentService;
+import com.lambdasys.insurance.payment.streaming.CheckoutProcessor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CheckoutCreatedListener {
+
+    private final CheckoutProcessor checkoutProcessor;
+
+    private final PaymentService paymentService;
+
+    @StreamListener(CheckoutProcessor.INPUT)
+    public void handler(CheckoutCreatedEvent checkoutCreatedEvent) {
+        log.info("checkoutCreatedEvent={}", checkoutCreatedEvent);
+        final PaymentEntity paymentEntity = paymentService.create(checkoutCreatedEvent).orElseThrow();
+        final PaymentCreatedEvent paymentCreatedEvent = PaymentCreatedEvent.newBuilder()
+                .setCheckoutCode(paymentEntity.getCheckoutCode())
+                .setPaymentCode(paymentEntity.getCode())
+                .build();
+        checkoutProcessor.output().send(MessageBuilder.withPayload(paymentCreatedEvent).build());
+    }
+}
